ffd7298a5453afce277bd97d3084a40dee10e327
jurlaub <j.urlaub@gmail.com>
2016-04-28 13:50:59 -0700

    Detail page with protected title and unified keylines

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
@@ -50,6 +50,9 @@ public class ArticleDetailFragment extends Fragment implements
     private ImageView mPhotoView;
     private String mTitleText;
 
+    private TextView mTitleView;
+    private TextView mByLineView;
+
 
     private int mScrollY;
     private boolean mIsCard = false;
@@ -104,25 +107,7 @@ public class ArticleDetailFragment extends Fragment implements
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         mRootView = inflater.inflate(R.layout.fragment_article_detail_2, container, false);
-//        mDrawInsetsFrameLayout = (DrawInsetsFrameLayout)
-//                mRootView.findViewById(R.id.draw_insets_frame_layout);
-//        mDrawInsetsFrameLayout.setOnInsetsCallback(new DrawInsetsFrameLayout.OnInsetsCallback() {
-//            @Override
-//            public void onInsetsChanged(Rect insets) {
-//                mTopInset = insets.top;
-//            }
-//        });
 
-//        mScrollView = (ObservableScrollView) mRootView.findViewById(R.id.scrollview);
-//        mScrollView.setCallbacks(new ObservableScrollView.Callbacks() {
-//            @Override
-//            public void onScrollChanged() {
-//                mScrollY = mScrollView.getScrollY();
-//                getActivityCast().onUpButtonFloorChanged(mItemId, ArticleDetailFragment.this);
-//                mPhotoContainerView.setTranslationY((int) (mScrollY - mScrollY / PARALLAX_FACTOR));
-//                updateStatusBar();
-//            }
-//        });
 
         mPhotoView = (ImageView) mRootView.findViewById(R.id.photo);
 //        mPhotoContainerView = mRootView.findViewById(R.id.photo_container);
@@ -157,12 +142,15 @@ public class ArticleDetailFragment extends Fragment implements
 
                 if (scrollRange + verticalOffset == 0  && mTitleText != null) {
                     collapsingToolbarLayout.setTitle(mTitleText);
+                    mTitleView.setVisibility(View.INVISIBLE);
                     isShow = true;
                 } else if (scrollRange + verticalOffset == 0) {
                     collapsingToolbarLayout.setTitle(getString(R.string.app_name));
+                    mTitleView.setVisibility(View.INVISIBLE);
                     isShow = true;
                 } else if (isShow) {
                     collapsingToolbarLayout.setTitle("");
+                    mTitleView.setVisibility(View.VISIBLE);
                     isShow = false;
                 }
             }
@@ -174,42 +162,16 @@ public class ArticleDetailFragment extends Fragment implements
         return mRootView;
     }
 
-//    private void updateStatusBar() {
-//        int color = 0;
-//        if (mPhotoView != null && mTopInset != 0 && mScrollY > 0) {
-//            float f = progress(mScrollY,
-//                    mStatusBarFullOpacityBottom - mTopInset * 3,
-//                    mStatusBarFullOpacityBottom - mTopInset);
-//            color = Color.argb((int) (255 * f),
-//                    (int) (Color.red(mMutedColor) * 0.9),
-//                    (int) (Color.green(mMutedColor) * 0.9),
-//                    (int) (Color.blue(mMutedColor) * 0.9));
-//        }
-//        mStatusBarColorDrawable.setColor(color);
-////        mDrawInsetsFrameLayout.setInsetBackground(mStatusBarColorDrawable);
-//    }
 
-//    static float progress(float v, float min, float max) {
-//        return constrain((v - min) / (max - min), 0, 1);
-//    }
-//
-//    static float constrain(float val, float min, float max) {
-//        if (val < min) {
-//            return min;
-//        } else if (val > max) {
-//            return max;
-//        } else {
-//            return val;
-//        }
-//    }
+
 
     private void bindViews() {
         if (mRootView == null) {
             return;
         }
 
-        TextView titleView = (TextView) mRootView.findViewById(R.id.article_title);
-        TextView bylineView = (TextView) mRootView.findViewById(R.id.article_byline);
+        mTitleView = (TextView) mRootView.findViewById(R.id.article_title);
+        mByLineView = (TextView) mRootView.findViewById(R.id.article_byline);
 //        bylineView.setMovementMethod(new LinkMovementMethod());
         TextView bodyView = (TextView) mRootView.findViewById(R.id.article_body);
 //        bodyView.setTypeface(Typeface.createFromAsset(getResources().getAssets(), "Rosario-Regular.ttf"));
@@ -220,21 +182,24 @@ public class ArticleDetailFragment extends Fragment implements
 //            mRootView.animate().alpha(1);
 
             mTitleText = mCursor.getString(ArticleLoader.Query.TITLE);
-            titleView.setText(mTitleText);
+            mTitleView.setText(mTitleText);
 
-            bylineView.setText(Html.fromHtml(
+            mByLineView.setText(Html.fromHtml(
                     DateUtils.getRelativeTimeSpanString(
                             mCursor.getLong(ArticleLoader.Query.PUBLISHED_DATE),
                             System.currentTimeMillis(), DateUtils.HOUR_IN_MILLIS,
                             DateUtils.FORMAT_ABBREV_ALL).toString()
-                            + " by <font color='#ffffff'>"
-                            + mCursor.getString(ArticleLoader.Query.AUTHOR)
-                            + "</font>"));
+                            + " by "
+                            + mCursor.getString(ArticleLoader.Query.AUTHOR)));
+
+//            <font color='#ffffff'> </font>
+
             bodyView.setText(Html.fromHtml(mCursor.getString(ArticleLoader.Query.BODY)));
 
 
             ImageLoaderHelper.getInstance(getActivity()).getImageLoader()
                     .get(mCursor.getString(ArticleLoader.Query.PHOTO_URL), new ImageLoader.ImageListener() {
+
                         @Override
                         public void onResponse(ImageLoader.ImageContainer imageContainer, boolean b) {
                             Bitmap bitmap = imageContainer.getBitmap();
@@ -255,8 +220,8 @@ public class ArticleDetailFragment extends Fragment implements
                     });
         } else {
             mRootView.setVisibility(View.GONE);
-            titleView.setText("N/A");
-            bylineView.setText("N/A" );
+            mTitleView.setText("N/A");
+            mByLineView.setText("N/A" );
             bodyView.setText("N/A");
         }
     }
@@ -291,14 +256,5 @@ public class ArticleDetailFragment extends Fragment implements
         bindViews();
     }
 
-//    public int getUpButtonFloor() {
-//        if (mPhotoContainerView == null || mPhotoView.getHeight() == 0) {
-//            return Integer.MAX_VALUE;
-//        }
-//
-//        // account for parallax
-//        return mIsCard
-//                ? (int) mPhotoContainerView.getTranslationY() + mPhotoView.getHeight() - mScrollY
-//                : mPhotoView.getHeight() - mScrollY;
-//    }
+
 }
